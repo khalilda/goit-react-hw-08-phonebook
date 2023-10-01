6c474e3726b6a436453f2a84876b70a9ea04c378
khalilda <100872154+khalilda@users.noreply.github.com>
2023-10-01 21:59:09 +0300

    hw

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -5,5 +5,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 export const PublicRoute = () => {
   const token = useSelector(getToken);
 
-  return token ? <Navigate to="/" /> : <Outlet />;
+  // return token ? <Navigate to="/" /> : <Outlet />;
+  return token ? <Navigate to="/contacts" /> : <Outlet />;
 };
